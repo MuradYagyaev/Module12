3604293799e9810ebd59bd3e347919dd988de77f
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-08-31 19:56:49 +0300

    Add files via upload

---
+++ runner_and_tournament_test.py
@@ -0,0 +1,42 @@
+import runner_and_tournament as tour
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.runner1 = tour.Runner('Усейн', 10)
+        self.runner2 = tour.Runner('Андрей', 9)
+        self.runner3 = tour.Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results:
+            for key, value in result.items():
+                result[key] = str(value)
+            print(result)
+
+    def test_tournament_1(self):
+        tournament1 = tour.Tournament(90, self.runner1, self.runner3)
+        results = tournament1.start()
+        self.all_results.append(results)
+        self.assertTrue(results[len(results)].__eq__('Ник'))
+
+    def test_tournament_2(self):
+        tournament2 = tour.Tournament(90, self.runner2, self.runner3)
+        results = tournament2.start()
+        self.all_results.append(results)
+        self.assertTrue(results[len(results)].__eq__('Ник'))
+
+    def test_tournament_3(self):
+        tournament3 = tour.Tournament(90, self.runner1, self.runner2, self.runner3)
+        results = tournament3.start()
+        self.all_results.append(results)
+        self.assertTrue(results[len(results)].__eq__('Ник'))
+
+
+if __name__ == '__main__':
+    unittest.main()
